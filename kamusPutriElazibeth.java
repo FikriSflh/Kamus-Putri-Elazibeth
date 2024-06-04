e24320f1e6ffa3dbec081176df7903f4cd5d4d9f
FikriSflh <148163834+FikriSflh@users.noreply.github.com>
2024-06-04 21:01:20 +0700

    Update kamusPutriElazibeth.java

--- kamusPutriElazibeth.java
+++ kamusPutriElazibeth.java
@@ -14,3 +14,12 @@ public class kamusPutriElazibeth {
                 table[i] = new ArrayList<>();
             }
         }
+    private int hashFunction(String kata) {
+            int hash = 0;
+            for (int i = 0; i < kata.length(); i++) {
+                char ch = kata.charAt(i);
+                int asciiValue = (int) ch;
+                hash = (hash * 31 + asciiValue) & 0xFFFFFFFF;
+            }
+            return Math.abs(hash) % size;
+        }       
